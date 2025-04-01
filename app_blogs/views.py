2f713dd1c0cef4b5fb72e82894188196b8d75dc6
Riya <iamriyasaryam@gmail.com>
2025-04-01 12:32:19 +0530

    created views and urls of every app

--- app_blogs/views.py
+++ app_blogs/views.py
@@ -1,9 +1,86 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm, PasswordResetForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm, PasswordResetForm, SubscriptionForm, RegistrationForm
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.views import view, ViewList
 
-# Home Page
-def home(request):
-    return render(request, 'blog/home.html')
+
+
+class HomeView(view):
+    def get(self, request):
+        return render(request, 'home.html')
+
+class BlogListView(ViewList):
+    def get(self, request):
+        blogs = Blog.objects.all()
+        return render(request, 'blog_list.html', {'blogs': blogs})
+    
+class BlogDetailView(view):
+    def get(self, request, blog_id):
+        blog = Blog.objects.get(id=blog_id)
+        return render(request, 'blog_detail.html', {'blog': blog})
+
+class SubscribeView(view):
+    def get(self, request):
+        form = SubscriptionForm()
+        return render(request, 'subscribe.html', {'form': form})
+
+    def post(self, request):
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Thank you for subscribing!")
+        return render(request, 'subscribe.html', {'form': form})
+    
+class RegisterView(view):
+    def get(self, request):
+        form = RegistrationForm()
+        return render(request, 'register.html', {'form': form})
+
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('login')
+        return render(request, 'register.html', {'form': form})
+
+class LoginView(view):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
+        return HttpResponse("Invalid credentials!")
+
+class LogoutView(view):
+    def get(self, request):
+        logout(request)
+        return redirect('home')
+
+class ProfileView(view):
+    def get(self, request):
+        return render(request, 'profile.html')
+
+class ContactView(view):
+    def get(self, request):
+        return render(request, 'contact.html')
+
+class AuthorView(view):
+    def get(self, request):
+        return render(request, 'author.html')
+
+class TermsView(view):
+    def get(self, request):
+        return render(request, 'terms.html')
+
+class PrivacyView(view):
+    def get(self, request):
+        return render(request, 'privacy.html')
--- app_portfolio/views.py
+++ app_portfolio/views.py
@@ -1,6 +1,64 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import SubscriptionForm
+from django.http import HttpResponse
+from django.views import view, viewList 
 
-# Home Page
-def home(request):
-    return render(request, 'home.html')
 
+# Views
+class HomeView(view):
+    def get(self, request):
+        return render(request, 'home.html')
+
+class SkillListView(viewList):
+    def get(self, request):
+        skills = Skill.objects.all()
+        return render(request, 'skill_list.html', {'skills': skills})
+
+class ProjectListView(viewList):
+    def get(self, request):
+        projects = Project.objects.all()
+        return render(request, 'project_list.html', {'projects': projects})
+
+class ProjectDetailView(viewList):
+    def get(self, request, project_id):
+        project = Project.objects.get(id=project_id)
+        return render(request, 'project_detail.html', {'project': project})
+
+class ExperienceListView(viewList):
+    def get(self, request):
+        experiences = Experience.objects.all()
+        return render(request, 'experience_list.html', {'experiences': experiences})
+
+class ExperienceDetailView(view):
+    def get(self, request, experience_id):
+        experience = Experience.objects.get(id=experience_id)
+        return render(request, 'experience_detail.html', {'experience': experience})
+
+class FAQListView(viewList):
+    def get(self, request):
+        faqs = FAQ.objects.all()
+        return render(request, 'faq_list.html', {'faqs': faqs})
+
+class ContactView(view):
+    def get(self, request):
+        return render(request, 'contact.html')
+
+class SubscribeView(view):
+    def get(self, request):
+        form = SubscriptionForm()
+        return render(request, 'subscribe.html', {'form': form})
+
+    def post(self, request):
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Thank you for subscribing!")
+        return render(request, 'subscribe.html', {'form': form})
+
+class TermsView(view):
+    def get(self, request):
+        return render(request, 'terms.html')
+
+class PrivacyView(view):
+    def get(self, request):
+        return render(request, 'privacy.html')
